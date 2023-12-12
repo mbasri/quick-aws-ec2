a708870665126c604ef33f7a4c3356ddb8b8de26
Mohamed BASRI <mbasri.dev@outlook.com>
2023-12-12 11:56:06 +0100

    Adding IAM Instance Profile

--- 06-iam-instance-profile.tf
+++ 06-iam-instance-profile.tf
@@ -1,4 +1,11 @@
 #---------------------------------------------------------------------------------------------------
 # IAM Instance Profile
 #---------------------------------------------------------------------------------------------------
-//TODO
+module "iam-instance-profile" {
+  source = "git::https://gitlab.com/mbasri-terraform/modules/aws/terraform-aws-iam-instance-profile?ref=v1.0.0"
+
+  iam_instance_profile_name = local.iam_instance_profile_name
+  description               = local.description
+
+  tags = local.tags
+}
